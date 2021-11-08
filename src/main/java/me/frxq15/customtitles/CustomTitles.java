2a2be6ed4670199e31374c35db38a931bdcbac82
CARTWRIGHT Dan <c019793k@student.staffs.ac.uk>
2021-11-08 18:47:04 +0000

    Initial commit

---
+++ src/main/java/me/frxq15/customtitles/CustomTitles.java
@@ -0,0 +1,17 @@
+package me.frxq15.customtitles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomTitles extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
