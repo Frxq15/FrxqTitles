93134e15ba89a2a77520408c10d9e71774855ebb
Frxq15 <DanCartwright123->
2021-11-22 20:41:23 +0000

    added shit

--- src/main/java/me/frxq15/customtitles/SQLManager/PlayerData.java
+++ src/main/java/me/frxq15/customtitles/SQLManager/PlayerData.java
@@ -1,4 +1,27 @@
 package me.frxq15.customtitles.SQLManager;
 
+import me.frxq15.customtitles.CustomTitles;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class PlayerData {
+    private final static Map<UUID, PlayerData> players = new HashMap<>();
+    private final UUID uuid;
+
+    public PlayerData(UUID uuid){
+        this.uuid = uuid;
+        players.put(uuid, this);
+    }
+    public UUID getUUID() { return uuid; }
+
+    public static PlayerData getPlayerData(CustomTitles Main, UUID uuid) {
+        if (!players.containsKey(uuid)) {
+            PlayerData playerData = new PlayerData(uuid);
+        }
+        return players.get(uuid);
+    }
+    public static Map<UUID, PlayerData> getAllPlayerData() { return players; }
+
 }
---
+++ src/main/java/me/frxq15/customtitles/Title.java
@@ -0,0 +1,9 @@
+package me.frxq15.customtitles;
+
+public class Title {
+    public int titleID;
+    public Title(int titleid) {
+        this.titleID = titleid;
+    }
+
+}
